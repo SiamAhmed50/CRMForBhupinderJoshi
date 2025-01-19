5010ca12b98ec9f65a7b23a365326eb28cec4e42
Siam Ahmed <siam@commercegenie.com>
2025-01-19 13:59:12 +0600

    obj files

--- CRM.Service/obj/Debug/net8.0/CRM.Service.AssemblyInfo.cs
+++ CRM.Service/obj/Debug/net8.0/CRM.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRM.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11ce196b05ad7daa8072a5ce586d76850dc213e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a3be94e6ac5fc09b786b0dee4d643c38bc23cb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRM.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRM.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
