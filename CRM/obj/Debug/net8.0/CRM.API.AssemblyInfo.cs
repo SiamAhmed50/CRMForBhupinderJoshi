c02b64efa1081002263155f1df62b2e1752b7af8
Siam Ahmed <siam@commercegenie.com>
2025-01-21 11:11:25 +0600

    commit files to merge code

--- CRM/obj/Debug/net8.0/CRM.API.AssemblyInfo.cs
+++ CRM/obj/Debug/net8.0/CRM.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRM.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5010ca12b98ec9f65a7b23a365326eb28cec4e42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0680afe429e972e1c282951fb0ef77eb3ba502b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRM.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRM.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
