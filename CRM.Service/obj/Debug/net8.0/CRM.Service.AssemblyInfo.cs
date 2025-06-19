0cb098a165de2e058931299035a1dc66d58eaf41
Siam Ahmed <siam@commercegenie.com>
2025-06-19 10:56:02 +0600

    uncommit changes

--- CRM.Service/obj/Debug/net8.0/CRM.Service.AssemblyInfo.cs
+++ CRM.Service/obj/Debug/net8.0/CRM.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRM.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62f1976d8d4b36b04f275e920c308d64612152ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a616c566c1db5f05978932f22a6acc44e28ca08")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRM.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRM.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
