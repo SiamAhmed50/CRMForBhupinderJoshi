3722f6462580de01f989f793031b7c989cfaf735
Siam Ahmed <siam@commercegenie.com>
2025-07-05 02:17:53 +0600

    localhost setup

--- CRM.Web/Program.cs
+++ CRM.Web/Program.cs
@@ -81,7 +81,7 @@ builder.Services.AddHttpClient("ApiClient", client =>
 {
 
     client.BaseAddress = new Uri("https://localhost:44300"); // Replace with your API base URL
-    //client.BaseAddress = new Uri("https://api-monitor.robobotics.eu"); // Replace with your API base URL
+                                                             // client.BaseAddress = new Uri("https://api-monitor.robobotics.eu"); // Replace with your API base URL
 }).AddHttpMessageHandler<CustomAuthorizationHandler>();
 
 var app = builder.Build();
